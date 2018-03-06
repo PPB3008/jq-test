6974c1b9cfbacfde3a00a9e8b049f281afaf4361
jack lee <deccsec@gmail.com>
2018-03-06 18:08:41 +0800

    v1.1 add some function

--- hamburger.js
+++ hamburger.js
@@ -1,25 +1,27 @@
 (function($){
 	$.fn.extend({
-		burger:function(docClass,aText){
+		burger:function(aText){
 			burger={
-				docClass:"",
 				aText:"",
 				doc:{},
 				state:false,
 				ulHeight:0,
-				getOption:function(docClass,aText){
-					this.docClass=docClass;
+				getOption:function(doc,aText){
+					this.doc=doc;
 					this.aText=aText;
 					return this.judge();
 				},
 				judge:function(){
-					if(document.querySelector(this.docClass)){
-					this.doc=document.querySelector(this.docClass);
+					if(this.doc){
 						for(let x in this.doc.childNodes){
 							if(this.doc.childNodes[x].nodeName=="UL"){
 								return this.add();
 							}
 						}
+					}
+					else{
+						console.log("new error");
+						return
 					}
 				},
 				add:function(){
@@ -31,28 +33,37 @@
 				},
 				render:function(ulDoc,aDoc){
 					let liDoc=document.querySelectorAll("."+this.doc.className+" li");
+					let top=this.ulTop(ulDoc);
 					this.doc.className+=" burger-container";
 					for(let y in liDoc){
 						if(liDoc[y].nodeName=="LI"){
 						this.ulHeight+=liDoc[y].offsetHeight;
 						}
+					}	
+					window.onresize=()=>{
+						this.state && this.overlayJudege(top,ulDoc);
+						
 					}
-					return this.addActive(ulDoc,aDoc);
+					return this.addActive(top,ulDoc,aDoc);
 				},
-				addActive:function(ulDoc,aDoc){
+				addActive:function(top,ulDoc,aDoc){
 					let ulClass=ulDoc.className;
 					let overlay=document.querySelector(".overlay");
 					aDoc.addEventListener("click",()=>{
+						document.querySelector("body").className="";
 						if(this.state){
 							document.querySelector(".overlay").className="overlay";
 							ulDoc.className=ulClass;
 							ulDoc.style.height=0;
+							ulDoc.style.overlay=""
 							this.state=false;
 						}
 						else{
 							document.querySelector(".overlay").className="overlay overlay-active";
+							document.querySelector("body").className+="body-overlay";
 							ulDoc.className=ulClass+" active";
-							ulDoc.style.height=burger.ulHeight+"px";
+
+							this.overlayJudege(top,ulDoc);
 							this.state=true;
 						}
 					})
@@ -62,9 +73,28 @@
 						ulDoc.style.height=0;
 						this.state=false;
 					})
+				},
+				ulTop:function(ulDoc){
+					let top=ulDoc.offsetTop;
+					let parent=ulDoc.offsetParent;
+					while(parent!=null){
+						top+=parent.offsetTop;
+						parent=parent.offsetParent;
+					}
+					return top;
+				},
+				overlayJudege:function(top,ulDoc){
+					if(window.innerHeight-top-this.ulHeight<0){
+						ulDoc.style.overflowY="scroll";
+						ulDoc.style.height=window.innerHeight-top+"px";
+					}
+					else{
+						ulDoc.style.overflowY="";
+						ulDoc.style.height=this.ulHeight+"px";
+					}
 				}
 			};
-			burger.getOption(docClass,aText);
+			burger.getOption(this.get(0),aText);
 			return this;
 		}
 	});
